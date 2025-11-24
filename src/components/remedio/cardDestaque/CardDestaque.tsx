6215a59b6161fa9a4a02ad178ed15d043bd8a0e0
Akanni <akanni029@gmail.com>
2025-11-24 14:24:14 -0300

    implementação da logica de apresentação do cardDestaque da Home

---
+++ src/components/remedio/cardDestaque/CardDestaque.tsx
@@ -0,0 +1,37 @@
+import type Remedio from "../../../models/Remedio";
+
+interface CardDestaqueProps {
+  remedio: Remedio;
+}
+
+function CardDestaque({ remedio }:CardDestaqueProps) {
+  return (
+   <div
+            className="bg-linear-to-r from-green-500 to-sky-500 
+                          text-white rounded-b-3xl shadow-xl p-6 -mt-6"
+          >
+            <p className="text-lg font-semibold opacity-90 tracking-wide">
+              PRÓXIMA DOSE:
+            </p>
+
+            <h2 className="text-4xl font-extrabold leading-tight drop-shadow">
+              {remedio.nome} {remedio.doseMg}
+            </h2>
+
+            <p className="text-base mt-1 mb-6 opacity-90">
+              1 Comprimido às {remedio.periodo?.horario}
+            </p>
+
+            <button
+              className="w-full bg-linear-to-r from-green-400 to-sky-400
+                        text-white text-2xl font-extrabold py-4
+                        rounded-full shadow-xl hover:scale-105
+                        transition-all duration-300"
+            >
+              TOMEI!
+            </button>
+          </div>
+  )
+}
+
+export default CardDestaque
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,40 +1,96 @@
+import { useNavigate } from "react-router-dom";
+import CardDestaque from "../../components/remedio/cardDestaque/CardDestaque";
 import ListarRemedios from "../../components/remedio/listarRemedios/ListarRemedios";
+import { useContext, useEffect, useState } from "react";
+import type Remedio from "../../models/Remedio";
+import { AuthContex } from "../../contexts/AuthContext";
+import { buscar } from "../../services/Service";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [remedios, setRemedios] = useState<Remedio[]>([]);
+
+  const { usuario, handleLogout } = useContext(AuthContex);
+  const token = usuario.token;
+
+  const remedioDestaque = getRemedioMaisProximo(remedios);
+
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado!");
+      navigate("/");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    buscarRemedios();
+  }, [remedios.length]);
+
+  async function buscarRemedios() {
+    try {
+      setIsLoading(true);
+
+      await buscar("/remedios", setRemedios, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function getRemedioMaisProximo(lista: Remedio[]): Remedio | null {
+    if (!lista || lista.length === 0) return null;
+
+    const agora = new Date();
+    const minutosAgora = agora.getHours() * 60 + agora.getMinutes();
+
+    let maisProximo: Remedio | null = null;
+    let menorDiferenca = Infinity;
+
+    lista.forEach((r) => {
+      if (!r.periodo?.horario) return;
+
+      const [h, m] = r.periodo.horario.split(":").map(Number);
+      const minutosRemedio = h * 60 + m;
+
+      // diferença considerando o próximo horário possível
+      let diff = minutosRemedio - minutosAgora;
+
+      // se já passou, considera amanhã
+      if (diff < 0) diff += 1440; // 24h
+
+      if (diff < menorDiferenca) {
+        menorDiferenca = diff;
+        maisProximo = r;
+      }
+    });
+
+    return maisProximo;
+  }
+
   return (
     <>
       <div className="bg-gray-50 flex justify-center min-h-screen relative">
         <div className=" p-4 flex flex-col gap-6">
-          {/* CARD DE DESTAQUE */}
-          <div
-            className="bg-linear-to-r from-green-500 to-sky-500 
-                          text-white rounded-b-3xl shadow-xl p-6 -mt-6"
-          >
-            <p className="text-lg font-semibold opacity-90 tracking-wide">
-              PRÓXIMA DOSE:
-            </p>
+          {/* Card Destaque */}
+          {!isLoading && !remedioDestaque && (
+            <span className="text-3xl text-center my-8">
+              Nenhum Remedio foi encontrado!
+            </span>
+          )}
 
-            <h2 className="text-4xl font-extrabold leading-tight drop-shadow">
-              Novalgina 500mg
-            </h2>
-
-            <p className="text-base mt-1 mb-6 opacity-90">
-              1 Comprimido às 10:00h
-            </p>
-
-            <button
-              className="w-full bg-linear-to-r from-green-400 to-sky-400
-                        text-white text-2xl font-extrabold py-4
-                        rounded-full shadow-xl hover:scale-105
-                        transition-all duration-300"
-            >
-              TOMEI!
-            </button>
-          </div>
-
+          {!isLoading && remedioDestaque && (
+            <CardDestaque remedio={remedioDestaque} />
+          )}
           {/* SEU DIA */}
           <h3 className="text-2xl font-bold text-gray-700 -mb-9">Seu Dia</h3>
-
           <ListarRemedios />
         </div>
       </div>
